73eb12c78534da864a03fc5ca648ade4f7ab52fa
bagewadiarti-hub <bagewadi.arti@gmail.com>
2026-03-11 12:47:59 +0530

    Create main.tf

---
+++ modules/vm/main.tf
@@ -0,0 +1,58 @@
+# modules/vm/main.tf
+# Reusable Azure VM Module
+
+resource "azurerm_public_ip" "vm_pip" {
+  for_each            = var.vms
+  name                = "pip-${each.key}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags                = var.tags
+}
+
+resource "azurerm_network_interface" "vm_nic" {
+  for_each            = var.vms
+  name                = "nic-${each.key}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tags                = var.tags
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_pip[each.key].id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  for_each            = var.vms
+  name                = "vm-${each.key}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  size                = each.value.vm_size
+  admin_username      = var.admin_username
+  tags                = var.tags
+
+  network_interface_ids = [
+    azurerm_network_interface.vm_nic[each.key].id
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.ssh_public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
