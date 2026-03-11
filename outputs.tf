33f4f6ba16b40a4a7a88bbdbe6431f9d455aa0c9
bagewadiarti-hub <bagewadi.arti@gmail.com>
2026-03-11 12:41:12 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,25 @@
+# modules/vm/outputs.tf
+
+output "vm_ids" {
+  description = "Map of VM names to their resource IDs"
+  value = {
+    for key, vm in azurerm_linux_virtual_machine.vm :
+    key => vm.id
+  }
+}
+
+output "private_ip_addresses" {
+  description = "Map of VM names to their private IP addresses"
+  value = {
+    for key, nic in azurerm_network_interface.vm_nic :
+    key => nic.private_ip_address
+  }
+}
+
+output "public_ip_addresses" {
+  description = "Map of VM names to their public IP addresses"
+  value = {
+    for key, pip in azurerm_public_ip.vm_pip :
+    key => pip.ip_address
+  }
+}
