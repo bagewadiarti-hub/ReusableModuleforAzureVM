89a4caa1db277f942275297787d0efa3c6c61049
bagewadiarti-hub <bagewadi.arti@gmail.com>
2026-03-11 12:40:39 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,45 @@
+# modules/vm/variables.tf
+
+variable "location" {
+  description = "Azure region where resources will be created"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "ID of the subnet where VMs will be deployed"
+  type        = string
+}
+
+variable "admin_username" {
+  description = "Admin username for the virtual machines"
+  type        = string
+  default     = "azureuser"
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key for VM authentication"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "vms" {
+  description = "Map of VM configurations - used for for_each"
+  type = map(object({
+    vm_size = string
+  }))
+  # Example:
+  # vms = {
+  #   "dev-01"     = { vm_size = "Standard_B1s" }
+  #   "staging-01" = { vm_size = "Standard_B2s" }
+  # }
+}
